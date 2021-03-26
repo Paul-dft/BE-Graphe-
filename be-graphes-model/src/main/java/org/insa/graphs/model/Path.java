44a720688824d64b0d65f8f14c595439c9e83dff
Duffaut Paul <duffaut@insa-20250>
2021-03-26 13:17:12 +0100

    Méthode getLength terminée.

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -210,11 +210,14 @@ public class Path {
      * 
      * @return Total length of the path (in meters).
      * 
-     * @deprecated Need to be implemented.
+     * deprecated Need to be implemented.
      */
     public float getLength() {
-        // TODO:
-        return 0;
+    	float length = 0;
+        for (Arc a : arcs) {
+			length  +=  a.getLength();
+        }
+        return length;
     }
 
     /**
@@ -225,11 +228,10 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
+     * deprecated Need to be implemented.
      */
     public double getTravelTime(double speed) {
-        // TODO:
-        return 0;
+    	return getLength() * 3600.0 / (speed * 1000.0);
     }
 
     /**
