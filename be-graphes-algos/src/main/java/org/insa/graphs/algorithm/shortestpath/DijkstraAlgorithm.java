caa2a98e74c17866722eecde6e23c0e9769f0976
Duffaut Paul <duffaut@localhost>
2021-05-09 15:16:33 +0200

    Avancement du Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -7,6 +7,7 @@ import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Label;
+import org.insa.graphs.model.Label_Nodes;
 import org.insa.graphs.model.Node;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
@@ -31,10 +32,38 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     	BinaryHeap<Label> heap ;
     	
     	Node origine = data.getOrigin();
+    	Label Label_origin ;
+    	Label_origin.sommet_courant = origine;
+    	Label_origin.cost = 0;
+    	Label_origin.pere = null;
     	
-    	heap.insert(origine);
+    	Graph graphe = data.getGraph();
     	
-    	while (verif == true) {
+    	Label[] LL;
+    	
+    	LL = new Label[graphe.size()];
+    	
+    	for(Node n : graphe.getNodes()) {
+    		
+    		Label l;
+    		l.cost = 1/0;
+    		l.pere = null;
+    		l.sommet_courant = n;
+    		LL[n.getId()] = l;
+    		
+    	}
+    	
+    	int var = origine.getId();
+    	LL[var] = Label_origin;
+    	
+    	
+    	for(Label l : LL) {
+    		
+    		heap.insert(l);
+    		
+    	}
+    	
+    	while (heap.isEmpty() == false) {
     		
     		destination = getSuccessors(findMin());
     		for (Arc arc : destination) {
